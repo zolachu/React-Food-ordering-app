f58e83be944108241d18f57ed4dcec8f1a962d5b
Zola <zolboo@alumni.stanford.edu>
2022-09-07 23:54:56 -0700

    finished app

--- src/components/Layout/HeaderCart.js
+++ src/components/Layout/HeaderCart.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "./HeaderCart.module.css";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,15 +6,31 @@ import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import CartContext from "../../store/cart-context";
 
 const HeaderCart = (props) => {
+  const [isBtnHighlighted, setBtnHighlighted] = useState(false);
+
   const context = useContext(CartContext);
 
   const numberOfItems = context.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+
+  const btnClass = `${styles.button} ${isBtnHighlighted ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (context.items.length === 0) return;
+    setBtnHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [context.items]);
+
   return (
     <Button
       variant="outline-dark"
-      className={styles.button}
+      className={btnClass}
       onClick={props.onShowCart}
     >
       <span> Your Cart </span>
